b0938f7f0ef6c70f89787ee17896e98b15ead002
CPOMalley <132834478+CPOMalley@users.noreply.github.com>
2024-05-21 21:06:36 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -137,6 +137,7 @@ function selectServices() {
 
   selectedServices.forEach(function (service) {
     var serviceContainer = document.createElement('div');
+    serviceContainer.classList.add('service-container');
     serviceContainer.innerHTML = `<h4>${service.charAt(0).toUpperCase() + service.slice(1)} Companies</h4>`;
     resultsContainer.appendChild(serviceContainer);
 
@@ -145,9 +146,10 @@ function selectServices() {
         getPlaceDetails(place, function (details) {
           calculateDistance(userLocation, details.geometry.location).then(function (distance) {
             var placeDetails = `
-              <div>
+              <div class="result-banner">
                 <input type="checkbox" class="company-checkbox" data-name="${details.name}" data-address="${details.vicinity}" data-phone="${details.formatted_phone_number || 'N/A'}" data-distance="${distance}">
-                <a href="https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(details.name)}&query_place_id=${details.place_id}" target="_blank">${details.name}</a> - ${details.vicinity}<br>
+                <a href="https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(details.name)}&query_place_id=${details.place_id}" target="_blank">${details.name}</a><br>
+                ${details.vicinity}<br>
                 Distance: ${distance}<br>
                 Phone: ${details.formatted_phone_number || 'N/A'}
               </div>`;
@@ -160,7 +162,6 @@ function selectServices() {
     });
   });
 
-  // Show the "Select Companies" button after services are selected
   var selectCompaniesButton = document.getElementById('select-companies-button');
   selectCompaniesButton.style.display = 'block';
 }
