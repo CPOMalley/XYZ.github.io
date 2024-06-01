b6ce97a21b8cbb4fb20373d670169b0a4dc131d0
CPOMalley <132834478+CPOMalley@users.noreply.github.com>
2024-06-01 00:15:09 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -161,9 +161,9 @@ function selectServices() {
                   <a href="https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(details.name)}&query_place_id=${details.place_id}" target="_blank">${details.name}</a><br>
                   ${website}
                   ${details.vicinity}<br>
-                  <strong>Rating:</strong> ${rating} ${userRatingsTotal}<br>
-                  <strong>Distance:</strong> ${distance}<br>
-                  <strong>Phone:</strong> ${details.formatted_phone_number || 'N/A'}
+                  <p><strong>Rating:</strong> ${rating} ${userRatingsTotal}</p>
+                  <p><strong>Distance:</strong> ${distance}</p>
+                  <p><strong>Phone:</strong> ${details.formatted_phone_number || 'N/A'}</p>
                 </div>
               </div>`;
             serviceContainer.innerHTML += placeDetails;
